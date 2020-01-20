93ec280efb5b0a3b9fd9384d07fbca7190d863f3
lobhaum <contato@eduardoluciano.com.br>
2020-01-20 14:20:38 -0300

    require marko + /views/lista/lista.marko

--- alura/Node.js/Introdução/src/app/rotas/rotas.js
+++ alura/Node.js/Introdução/src/app/rotas/rotas.js
@@ -1,25 +1,13 @@
 module.exports = (app)=>{
     app.get('/', function (req, res) {
-        let html = `<html>
-        <head>
-            <meta charset="utf-8"/>
-        </head>
-        <body>
-            <h1>Bem-vindo à Livraria</h1>
-        </body>
-    </html>`
-        res.send(html)
+        res.marko(
+            require('../views/livros/lista/lista.marko')
+        )
     })
     
     app.get('/livros', function (req, res) {
-        let html = `<html>
-        <head>
-            <meta charset="utf-8"/>
-        </head>
-        <body>
-            <h1>Listagem de livros</h1>
-        </body>
-    </html>`
-        res.send(html)
+        res.marko(
+            require('../views/livros/lista/lista.marko')
+        )
     })
 }
