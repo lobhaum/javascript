276827c5bf59e8707341d135d93df63a7627fcb0
lobhaum <contato@eduardoluciano.com.br>
2020-01-20 14:45:29 -0300

    alteração do lista.marko, novamente fora do path

--- alura/Node.js/Introdução/src/app/rotas/rotas.js
+++ alura/Node.js/Introdução/src/app/rotas/rotas.js
@@ -7,7 +7,20 @@ module.exports = (app)=>{
     
     app.get('/livros', function (req, res) {
         res.marko(
-            require('../views/livros/lista/lista.marko')
+            require('../views/livros/lista/lista.marko'),
+            
+        {
+            livros: [
+                { 
+                    id: 1,
+                    titulo: 'Fundamentos do Node'
+                },
+                { 
+                    id: 2,
+                    titulo: 'Node Avançado'
+                }
+            ]
+        }
         )
     })
 }
