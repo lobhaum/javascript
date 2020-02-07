e1a3eb569d6d5ced2583b580c3093ca2cf352446
lobhaum <contato@eduardoluciano.com.br>
2020-02-07 09:38:16 -0300

    calculado quantidade de produtos p churrasco

--- churrasco/webapp/static/app/script.js
+++ churrasco/webapp/static/app/script.js
@@ -1,15 +1,55 @@
-function getFormulario(pessoas, horas){
-    let botao = document.querySelector('#botao')
-    botao.addEventListener('click', function(event){
-        event.preventDefault()
-        let form = document.querySelector('#getFormulario')
 
-    })
-    let base = {
-        "pessoas": document.querySelector('#pessoas'),
-        "horas": document.querySelector('#horas')
 
+/**
+ * Captura do botão do formulário
+ */
+// armazendo botao na variavel b
+let b = document.querySelector("#botao");
+// ouvindo o click de b
+b.addEventListener("click", function(event){
+    // prevendo reload da pagina ao clicar o botão
+    event.preventDefault();
+    // pega os dados do formulário
+    let dados = getData();
+    let churrasco = calculaChurrasco(dados);    
+
+    //debug
+    console.log("cliquei no botao")
+    console.log(dados)
+    console.log(churrasco)
+})
+/**
+ * Captura dos dados do formulário
+ */
+function getData(){
+    let retorno = {
+        "pessoas": document.querySelector("#pessoas").value,
+        "horas": document.querySelector("#horas").value
     }
-    return base
-
+    return retorno;
 }
+/**
+ * Calculando churrasco
+ */
+//calculando churrasco
+    /**
+     * 1 pessoa consome 300g carne por h
+     * come 3 pães
+     * toma 4 latinhas de cerveja
+     * toma 200ml de refrigerante
+     */
+    function calculaChurrasco(dados){
+        const carne = 300
+        const paes = 3
+        const cerveja = 4
+        const refrigerante = 200
+
+        let churrasco = {
+            "totalCarne": carne * dados.pessoas * dados.horas,
+            "totalPaes": paes * dados.pessoas * dados.horas,
+            "totalCerveja": cerveja * dados.pessoas * dados.horas,
+            "totalRefrigerante": refrigerante * dados.pessoas * dados.horas
+        }
+
+        return churrasco;
+    }
